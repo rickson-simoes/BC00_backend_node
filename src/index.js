5f0ca8f952e860b26d57e8dc92a89d9e2ec362e2
Rickson <rickson.simoes@hotmail.com>
2020-04-11 16:15:45 -0300

    :package: Pacote de ID, :hammer: Criacao do crud com query search no read

--- src/index.js
+++ src/index.js
@@ -1,23 +1,62 @@
 const express = require('express');
+const { uuid } = require('uuidv4');
 const app = express();
 app.use(express.json());
 
 const techs = [];
 
 app.get('/tech', (req, res) => {
-  return res.json(techs);
+  const { search } = req.query;
+
+  const resultado = search ? techs.filter(fil=> fil.description.includes(search)) : techs;
+  
+  return res.json(resultado);
 });
 
 app.post('/tech', (req, res) => {
-  return res.json();
+  const { name, description } = req.body;
+
+  const tech = { id: uuid(), name, description };
+
+  techs.push(tech);
+
+  return res.json(tech);
 });
 
 app.put('/tech/:id', (req, res) => {
-  return res.json();
+  const { id } = req.params;
+  const { name, description } = req.body;
+
+  const techIndex = techs.findIndex( f => f.id == id);
+
+  if (techIndex < 0) {
+    return res.status(404).json({ message : 'Id not found'});
+  }
+
+  const tech = {
+    id,
+    name,
+    description
+  }
+
+  techs[techIndex] = tech;
+
+  return res.json(tech);
 });
 
 app.delete('/tech/:id', (req, res) => {
-  return res.json();
+  const { id } = req.params;
+  const { name, description } = req.body;
+
+  const techIndex = techs.findIndex( f => f.id == id);
+
+  if (techIndex < 0) {
+    return res.status(404).json({ message : 'Id not found'});
+  }
+
+  techs.splice(techIndex, 1);
+
+  return res.status(200).json({ message : 'successful deleted'});
 });
 
 
